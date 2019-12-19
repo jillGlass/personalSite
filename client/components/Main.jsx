97e66face5d7391e0b23af7ec2b131448bb352c3
JillGlass <glass.nz@gmail.com>
2019-12-19 21:45:42 +1300

    create main component

---
+++ client/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export class Main extends Component {
+    render() {
+        return (
+            <div>
+                This is the main page and it should hold the grid.
+            </div>
+        )
+    }
+}
+
+export default Main

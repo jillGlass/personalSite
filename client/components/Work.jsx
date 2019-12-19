0bc602f7d325c90736e76730fb97e7abde207ad9
JillGlass <glass.nz@gmail.com>
2019-12-19 22:10:17 +1300

    create blog, work, about components

---
+++ client/components/Work.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Work extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Work

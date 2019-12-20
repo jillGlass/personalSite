d9937b8cd283f169affeb2a6f87b56466de01ec5
JillGlass <glass.nz@gmail.com>
2019-12-20 22:12:50 +1300

    add router and links

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,56 +1,57 @@
 import React from "react";
+import { Link, HashRouter as Router } from "react-router-dom";
 
 class Main extends React.Component {
   render() {
     return (
-      <div>
-        <div className="container">
-          <section className="column-1">
-            <a href="blog/blog.html">
-              {" "}
-              <header className="title">Blog</header>
-            </a>
-            <a href="blog/blog.html">
-              <p className="intro">Technical and learning reflections</p>
-            </a>
-            <div className="image">
-              <a href="blog/blog.html">
-                <img src="images/blogfancy.svg" alt="blog icon" height="140" />
-              </a>
+      <React.Fragment>
+        <Router>
+          <div className="container">
+            <div className="column-1">
+              <Link to="/blog">
+                <div className="title">Blog</div>
+                <p className="intro">Technical and learning reflections</p>
+                <div className="image">
+                  <img
+                    src="images/blogfancy.svg"
+                    alt="blog icon"
+                    height="140"
+                  />
+                </div>
+              </Link>
             </div>
-          </section>
-          <section className="column-2">
-            <a href="Work/work.html">
-              {" "}
-              <header className="title">Work</header>
-            </a>
-            <a href="Work/work.html">
-              <p className="intro">Examples of projects and tinkerings</p>
-            </a>
-            <div className="image">
-              <a href="Work/work.html">
-                <img src="images/workfancy.svg" alt="blog icon" height="140" />
-              </a>
+            <div className="column-2">
+             <Link to="/work">
+              <div className="title">Work</div>
+                <p className="intro">Examples of projects and tinkerings</p>
+                  <div className="image">
+                    <img
+                      src="images/workfancy.svg"
+                      alt="blog icon"
+                      height="140"
+                    />
+                 </div>
+             </Link>
             </div>
-          </section>
-          <section className="column-3">
-            <a href="About/about.html">
-              {" "}
-              <header className="title">About</header>
-            </a>
-            <a href="About/about.html">
-              <p className="intro">Contact details and a little about me</p>
-            </a>
-            <div className="image">
-              <a href="About/about.html">
-                <img src="images/aboutfancy.svg" alt="blog icon" height="140" />
-              </a>
+            <div className="column-3">
+              <Link to="/about">
+                <div className="title">About</div>
+                <p className="intro">Contact details and a little about me</p>
+                  <div className="image">
+                    <img
+                      src="images/aboutfancy.svg"
+                      alt="blog icon"
+                      height="140"
+                    />
+                  </div>
+              </Link>
             </div>
-          </section>
-        </div>
-      </div>
-    );
+            </div>
+        </Router>
+      </React.Fragment>
+    )
   }
 }
 
-export default Main;
+export default Main
+

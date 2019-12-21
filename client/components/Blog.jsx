a75dccd5b7d38dd6d333bdd754f673823be5d718
JillGlass <glass.nz@gmail.com>
2019-12-21 15:38:51 +1300

    blog page added

--- client/components/Blog.jsx
+++ client/components/Blog.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import NavInner from './NavInner'
+import React from "react";
+import NavInner from "./NavInner";
 
 class Blog extends React.Component {
-    render() {
-        return (
-            <div>
-                <NavInner />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <NavInner />
+        <div className="container-blog blog-background">
+          <header className="title">
+            <img src="../images/blogfancy.svg" alt="blog icon" height="130" />
+          </header>
+          <h3 className="blog-entry-title">
+            August 2019 - EDA Foundations
+          </h3>
+          </div>
+      </div>
+    );
+  }
 }
 
-export default Blog
+export default Blog;

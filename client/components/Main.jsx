93480a13e72f88f72b58b966b37584cd9b66f106
JillGlass <glass.nz@gmail.com>
2020-01-03 10:55:30 +1300

    add componentDidMount to fix issue of scrolling

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Link, HashRouter as Router } from "react-router-dom";
 
 class Main extends React.Component {
+
+  componentDidMount() {
+    window.scrollTo(0, 0);
+}
+
   render() {
     return (
       <React.Fragment>

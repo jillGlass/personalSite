49d26c04d1224c2609c25ea022293aef088a919a
JillGlass <glass.nz@gmail.com>
2020-02-17 13:04:25 +1300

    add footer links

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -48,7 +48,10 @@ class Main extends React.Component {
           </div>
           <div className="bottom-row">
             <div className="center-name-copy">glass.nz@gmail.com</div>
-            <div className="center-name-secondary icon-padding"><img src='images/icons8-linkedin-circled-48.png' height='37'/><img src='images/icons8-github-48.png' height='37'/></div>
+            <div className="center-name-secondary icon-padding">
+              <a href='https://www.linkedin.com/in/jillglass/'> <img src="images/icons8-linkedin-circled-48.png" height="37" /> </a>
+              <a href='https://github.com/jillGlass'><img src="images/icons8-github-48.png" height="37" /> </a>
+            </div>
           </div>
         </Router>
       </React.Fragment>
